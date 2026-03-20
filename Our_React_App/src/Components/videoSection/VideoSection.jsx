d5a144045f6b6fb9c612e5bc57c5a46e83230336
DaVoe <davemcthunder@gmail.com>
2026-03-20 03:47:39 +0100

    Refactor code structure for improved readability and maintainability

--- Our_React_App/src/Components/videoSection/VideoSection.jsx
+++ Our_React_App/src/Components/videoSection/VideoSection.jsx
@@ -1,5 +1,45 @@
 import React from 'react';
+import '../videoSection/VideoSection.css';
+import videoPlaceHolder from '../../assets/video-poster.jpg';
+import earthVideo from '../../assets/planet-vid.mp4';
+const VideoSection = () => {
 
-const VideoSection = () => null;
+  return (
+    <section className="video-section">
+        <div className="container video-grid">
+            <div className="video-column">
+                <div className="video-container">
+                    <video
+                        autoPlay
+                        muted
+                        loop
+                        playsInline
+                        className='video-element'>
+                            <source src={earthVideo} type='video/mp4' />
+                            Your browser does not support the video tag.
+                        </video>
+
+                        <div className="play-overelay">
+                            <div className="play-triangle"></div>
+                        </div>
+                </div>
+            </div>
+
+            <div className="text-column">
+                <h2>How Planetary Data Helps Us Understand Space</h2>
+                <p>
+                    Planetary science goes beyond images. Comparing
+                    <span className="highlight"> mass</span>,
+                    <span className="highlight"> diameter</span>,
+                    <span className="highlight"> gravity</span>, and
+                    <span className="highlight"> density</span>,
+                    we gain insight imto how palnets form, behave, and interact
+                    within the solar system.
+                </p>
+            </div>
+        </div>
+    </section>
+  )
+}
 
 export default VideoSection;
