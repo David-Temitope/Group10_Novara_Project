1ead0ccfaec144c87aa97a11b94a0e6522af6990
OLUWASEUN4321 <otiwax@gmail.com>
2026-03-28 09:48:10 +0100

    Created footer section with JSX and CSS
    I worked on the footer section, both the CSS and JSX part of the footer section.

--- Our_React_App/src/Components/footer/Footer.jsx
+++ Our_React_App/src/Components/footer/Footer.jsx
@@ -1,11 +1,42 @@
-import React from 'react'
+import React from 'react';
+import './Footer.css';
 
-const footer = () => {
+const Footer = () => {
+  const currentYear = 2026;
+  const groupRepoUrl = "https://github.com/David-Temitope/Group10_Novara_Project.git";
+
   return (
-    <div>
-        
-    </div>
-  )
-}
+    <footer id="Form" className="main-footer">
+      <div className="footer-content">
+        <div className="footer-about">
+          <h3>About</h3>
+          <p className="team-description">
+            We are a team of dedicated developers collaborating on 
+            planetary data visualization.
+          </p>
+          <div className="team-members">
+            <span>David, Victor, Daang, Oluwasemilore, Oluwaseun</span>
+          </div>
+        </div>
 
-export default footer
+        <div className="footer-divider"></div>
+
+        <div className="footer-bottom">
+          <div className="copyright-info">
+            <p>©{currentYear} Design by <a href="https://github.com/Anurella" target="_blank" rel="noreferrer">Amaka</a> &
+            <a href="https://www.linkedin.com/in/ifeomaokocha" target="_blank" rel="noreferrer">Ifeoma A</a>.</p>
+            <p>
+              Built by <a href={groupRepoUrl} className="group-link">TS Academy Group 10</a>. 
+              All rights reserved
+            </p>
+          </div>
+          <div className="footer-brand">
+            <a href="https://tsacademyonline.com/" target="_blank" rel="noreferrer">TSAcademy</a>
+          </div>
+        </div>
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
